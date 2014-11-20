d36cca93265ccda031572e45b33dc638b4d7b89b
Theron J Hitchman <theronhitchman@gmail.com>
2014-11-20 05:23:48 +0000

    update files after testing

--- examples/python_script.py
+++ examples/python_script.py
@@ -1,7 +1,7 @@
 # find the sum of all numbers less than 1000 which are multiples of 3 and 5
 listy = []
-for x in srange(0,1000):
+for x in xrange(0,1000):
     if x % 3 == 0 or x % 5 ==0:
         listy.append(x)
 
-sum(listy)
+print sum(listy)
